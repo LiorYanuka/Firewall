9cc1e1ea475e7dd724990da9c5de03529552968c
LiorYanuka <yanukalior@gmail.com>
2025-08-24 19:07:37 +0300

    Added Postgres to main

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import app from "./server"
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
+
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import express, { Request, Response } from "express"
+import { query } from "./db";
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Server is running just fine!");
 })
+
+// app.get("/users", async (req, res) => {
+//   try {
+//     const result = await query("SELECT * FROM users");
+//     res.json(result.rows);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ error: "Database error" });
+//   }
+// });
 
 export default app;
